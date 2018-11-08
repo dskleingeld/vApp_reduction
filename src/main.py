677d849f73473c6188d2eee1e43ddc6c2593ce88
David Kleingeld <dskleingeld@gmail.com>
2018-11-08 17:37:56 +0100

    pipeline working

--- src/main.py
+++ src/main.py
@@ -1,59 +1,44 @@
-# general program layout, needs more thought mainly:
-# - how does this scape up for running multiple runs with diff paramaters?
-# - how can an auto analasis/quantifying procedure be included?
-# - future multithreading?
-
-    #get disk
-        #set disk/object properties, get obj/disk class
-    
-    #process, 
-        #load/create psf and get its dimensions
-    
-    #rdi
-        #generate single complete psf's, this code lives in the psf modele:
-            #generate the (disk) non spreaded image
-            #convolve the disk with the psf 
-        #generate proc
-        
-        
-        #return both processed and generated input(s)?
-    
-    #adi
-        #generate multiple complete psf's, this code lives in the psf modele:
-            #generate the (disk) non spreaded image
-            #convolve the disk with the psf 
-        #generate processed image
-        #return both processed and generated input(s)? 
-    
-    #plot
-        #(optional) matplotlib for report printing
-        #something faster and better for experimenting
-
 import psf.from_simulated_psf as psf
 import disk as d
 import plot.fast as plotfast
 import plot.slow as plotslow
+import numpy as np
 from scipy import signal
 from hcipy import * 
 
-    #https://github.com/spacetelescope/asdf
+    #TODO rewrite pipline so params rotation and time are stored next 
+    # to the data in a dict
 def adi(angular_seperation, time_between_exposures, numb):
     
     #set disk properties
-    clean_disk = d.disk(10, with_star=False, inner_radius=1,
-                 outer_radius=3, rotation=135, inclination=70)
+    disk_without_star = d.disk(10, with_star=False, inner_radius=1,
+                        outer_radius=3, rotation=135, inclination=70)
+    disk_with_star = d.disk(10, with_star=True, inner_radius=1,
+                     outer_radius=3, rotation=135, inclination=70)
     
-    #create disk cube
-    #disk_cube = d.gen_cube(clean_disk, angular_seperation, numb)
-    
+    #process disk without center star
+#    #create disk cube
+    (disk_cube, disk_params) = d.gen_cube(numb, disk_without_star, angular_seperation)
+    plotfast.image(disk_cube[0])
     #lazily create psf cube
-    psf_cube = psf.calc_cube() #fix in ultra ugly way
-    
+    (psf_cube, psf_params) = psf.calc_cube(numb, fried_parameter=4, time_between=0.7)
     #lazily convolve signals
-    image_cube = process_clean(psf_cube, disk_cude)
+    (img_cube_without_star, img_params) = psf.convolve(psf_cube, disk_cube, psf_params, disk_params)
+    
+    #process disk with center star
+    #create disk cube
+    (disk_cube, disk_params) = d.gen_cube(numb, disk_with_star, angular_seperation)
+    plotfast.image(disk_cube[0])
+    #lazily create psf cube
+    (psf_cube, psf_params) = psf.calc_cube(numb, fried_parameter=4, time_between=0.7)
+    #lazily convolve signals
+    (img_cube_with_star, img_params) = psf.convolve(psf_cube, disk_cube, psf_params, disk_params)
     
     #do adi
-    #plotfast.image(clean_disk)
+    mean = np.mean(img_cube_with_star, axis=0)
+    
+    plotfast.image(img_cube_with_star-mean)
+    
     
 if __name__ == "__main__":
     adi(10, 0.7, 10)
