366e4bff729b303c648b1f7df4d68a615ddaa49a
David Kleingeld <dskleingeld@gmail.com>
2019-05-11 17:23:28 +0200

    add function to generate clean vApp psf from ampl and phase file

--- src/experiments/miscellaneous.py
+++ src/experiments/miscellaneous.py
@@ -141,3 +141,69 @@ def run_loyt():
     imshow_field(np.log10(img_ref.intensity / img_ref.intensity.max()), vmin=-12)
     plt.colorbar()
     fig.savefig(output_path+"normal_psf")
+
+def clean_vapp():
+    import numpy as np
+    import matplotlib.pyplot as plt 
+
+    from hcipy import read_fits, make_pupil_grid, make_focal_grid, FraunhoferPropagator, circular_aperture, evaluate_supersampled, imshow_field, imsave_field, Field, LyotCoronagraph, Wavefront
+
+    script_path = os.path.realpath(__file__).split("vApp_reduction",1)[0]
+    full_path = script_path+"vApp_reduction/data/"
+    amplitude_file = full_path+'SCExAO_vAPP_amplitude_resampled.fits'
+    phase_file     = full_path+'SCExAO_vAPP_phase_resampled.fits'
+
+    # loading the files required for generating the vAPP. 
+    amplitude_temp = read_fits(amplitude_file)
+    phase_temp     = read_fits(phase_file)
+
+    # number of pixels along one axis in the pupil
+    Npix = amplitude_temp.shape[0]
+
+    # generating the grids 
+    pupil_grid = make_pupil_grid(Npix)
+    focal_grid = make_focal_grid(pupil_grid, 4, 25)
+
+    # Mapping from pupil plane to focal plane
+    propagator = FraunhoferPropagator(pupil_grid, focal_grid)
+
+    # converting the amplitude and phase to fields 
+    amplitude = Field(amplitude_temp.ravel(), pupil_grid)
+    phase     = Field(phase_temp.ravel(), pupil_grid)
+
+    # the wavefront for the three PSFs 
+    pupil_wf_PSF_1 = Wavefront(amplitude * np.exp(1j * phase)) # coronagraphic PSF 1
+    pupil_wf_PSF_2 = Wavefront(amplitude * np.exp(-1j * phase)) # coronagraphic PSF 2
+    pupil_wf_PSF_3 = Wavefront(amplitude) # leakage
+
+    # Propagating them to the focal plane 
+    focal_wf_PSF_1 = propagator(pupil_wf_PSF_1)
+    focal_wf_PSF_2 = propagator(pupil_wf_PSF_2)
+    focal_wf_PSF_3 = propagator(pupil_wf_PSF_3)
+
+    # setting the total power in the images 
+    focal_wf_PSF_1.total_power = 1
+    focal_wf_PSF_2.total_power = 1
+    focal_wf_PSF_3.total_power = 1
+
+    # the strenght of the leakage term
+    leakage = 0.02
+
+    # getting the power and scaling with leakage 
+    PSF_1_pwr = focal_wf_PSF_1.power * (1 - leakage / 2)
+    PSF_2_pwr = focal_wf_PSF_2.power * (1 - leakage / 2)
+    PSF_3_pwr = focal_wf_PSF_3.power * leakage
+
+    # generating the total PSF of the vAPP
+    total_PSF = PSF_1_pwr + PSF_2_pwr + PSF_3_pwr
+
+    # normalizing to sum = 1
+    total_PSF /= np.sum(total_PSF)
+
+    fig = plt.figure()
+    imshow_field(np.log10(total_PSF / total_PSF.max()), vmin=-5)
+    plt.colorbar()
+
+    plt.show()
+    output_path = get_output_path("miscellaneous")
+    fig.savefig(output_path+"clean_vapp")
